96c0d70d936ffda0706e04f1b143517e6aa17c7c
hnnav <57915266+hnnav@users.noreply.github.com>
2022-10-28 12:20:43 +0300

    fetching

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { BrowserRouter, Routes, Route, useParams } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import './App.css';
 import Navbar from './components/Navbar'
 import Home from './components/Home'
@@ -9,32 +9,21 @@ export default function App() {
 
   const [countries, setCountries] = useState([]);
 
-  // Fetch all countries
-  const getApiData = async () => {
-    const response = await fetch(
-      "https://restcountries.com/v3.1/all"
-    ).then((response) => response.json());
+  const fetchAllCountries = async () => {
+    const response = await fetch("https://restcountries.com/v3.1/all")
+    .then((response) => response.json());
     setCountries(response);
   };
   useEffect(() => {
-    getApiData();
+    fetchAllCountries();
   }, []);
-
-  // Get Country Name
-	const getCountryName = (code) => {
-		return countries
-			.filter((element) => {
-				return element.cca3 === code;
-			})
-			.map((el, i) => el.name.common);
-	};
 
   return (
     <BrowserRouter>
       <Navbar />
       <Routes>
         <Route path="/" element={<Home countries={countries} />} />
-        <Route path='/:countryName' element={<Country getCountryName={getCountryName}/>} />
+        <Route path='/:countryName' element={<Country />} />
       </Routes>
     </BrowserRouter>
   );
--- src/components/Country.js
+++ src/components/Country.js
@@ -1,32 +1,33 @@
-import { Link } from "react-router-dom";
 import { useParams } from "react-router";
 import { useEffect, useState } from "react";
 
-export default function Country({ getCountryName }) {
+export default function Country() {
 
+    const [country, setCountry] = useState([]);
     const countryName = useParams();
-	const [country, setCountry] = useState([]);
-
-    const fetchHandler = async () => {
-		fetch(
-			`https://restcountries.com/v3.1/name/${countryName.countryName}`
-		)
-			.then((response) => response.json())
-			.then((data) => {
-				setCountry(data[0]);
-			})
-			.catch((error) => {
-				console.log(error);
-			});
-	};
+    
+    const fetchOneCountry = async () => {
+        const response = await fetch(`https://restcountries.com/v3.1/name/${countryName.countryName}`)
+        .then((response) => response.json())
+        .then((data) => setCountry(data[0]))
+    };
     useEffect(() => {
-		fetchHandler();
-	}, [countryName]);
-
+        fetchOneCountry(); // eslint-disable-next-line
+	}, []);
+    console.log(country)
+    
     return (
         <div>
-            <p>Population: {country.population}</p>
-            <p>Region: {country.region}</p>
+            <h1>{/* {console.log(country.name.common)} */}</h1>
+            <p><b>Native Name:</b> {country.name.official}</p>
+            <p><b>Population:</b> {country.population}</p>
+            <p><b>Region:</b> {country.region}</p>
+            <p><b>Subregion:</b> {country.subregion}</p>
+            <p><b>Capital:</b> {country.capital}</p>
+            <p><b>Top level domain:</b> {country.tld}</p>
+            <p><b>Currencies:</b> {/* {country.currencies.bbd.name} */}</p>
+            <p><b>Languages:</b> {/* {country.languages[0]} */}</p>
+            <p><b>Border countries:</b> {/* {country.} */}</p>
         </div>
     )
 }
