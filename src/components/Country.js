829c077d336ef524301f7fcd9f008c946e862bb6
hnnav <57915266+hnnav@users.noreply.github.com>
2022-10-28 14:11:10 +0300

    css

--- src/components/Country.js
+++ src/components/Country.js
@@ -1,10 +1,12 @@
 import { useParams } from "react-router";
 import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
 
 export default function Country() {
 
     const [country, setCountry] = useState([]);
     const countryName = useParams();
+
     
     const fetchOneCountry = async () => {
         const response = await fetch(`https://restcountries.com/v3.1/name/${countryName.countryName}`)
@@ -14,20 +16,32 @@ export default function Country() {
     useEffect(() => {
         fetchOneCountry(); // eslint-disable-next-line
 	}, []);
-    console.log(country)
     
     return (
-        <div>
-            <h1>{/* {console.log(country.name.common)} */}</h1>
-            <p><b>Native Name:</b> {country.name.official}</p>
-            <p><b>Population:</b> {country.population}</p>
-            <p><b>Region:</b> {country.region}</p>
-            <p><b>Subregion:</b> {country.subregion}</p>
-            <p><b>Capital:</b> {country.capital}</p>
-            <p><b>Top level domain:</b> {country.tld}</p>
-            <p><b>Currencies:</b> {/* {country.currencies.bbd.name} */}</p>
-            <p><b>Languages:</b> {/* {country.languages[0]} */}</p>
-            <p><b>Border countries:</b> {/* {country.} */}</p>
+        <div className="country-detail">
+            <Link to="/">
+                <button className="back-button"><ion-icon name="arrow-back-outline"></ion-icon> Back</button>
+            </Link>
+            <img className="detail-page-flag" src={country.name ? country.flags.png : null}></img>
+
+            <div className="detail-page-text">
+                <h1>{country.name ? country.name.common : null}</h1>
+                <p><b>Native Name:</b> {country.name ? Object.entries(country.name.nativeName).map((item) => {
+			    										return item[1].official;
+			    									}) : null}</p>
+                <p><b>Population:</b> {country.population}</p>
+                <p><b>Region:</b> {country.region}</p>
+                <p><b>Subregion:</b> {country.subregion}</p>
+                <p><b>Capital:</b> {country.capital}</p>
+                <p><b>Top level domain:</b> {country.tld}</p>
+                <p><b>Currencies:</b> {country.currencies ? Object.entries(country.currencies).map((item) => {
+			    										return item[1].name;
+			    									}) : null}</p>
+                <p><b>Languages:</b> {country.languages ? Object.entries(country.languages).map((item) => {
+			    										return item[1];
+			    									}) : null}</p>
+                <p><b>Border countries:</b> </p>
+            </div>
         </div>
     )
 }
