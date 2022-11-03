8bb50d1d15e2f0fe1643ba68073a721f04a58f78
hnnav <57915266+hnnav@users.noreply.github.com>
2022-11-03 10:09:06 +0200

    darkmode

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import React, { useEffect, useState } from 'react';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import './App.css';
+import './darkMode.css';
 import Navbar from './components/Navbar'
 import Home from './components/Home'
 import Country from './components/Country'
 
 export default function App() {
 
+  // Fetch all countries
   const [countries, setCountries] = useState([]);
-
+  
   const fetchAllCountries = async () => {
     const response = await fetch("https://restcountries.com/v3.1/all")
     .then((response) => response.json());
@@ -18,9 +20,22 @@ export default function App() {
     fetchAllCountries();
   }, []);
 
+  // Dark / Light mode
+  const [theme, setTheme] = useState('light');
+
+  const toggleTheme = () => {
+    if (theme === 'light') {
+      setTheme('dark');
+    } else {
+      setTheme('light');
+    }};
+  useEffect(() => {
+    document.body.className = theme;
+  }, [theme]);
+
   return (
-    <BrowserRouter>
-      <Navbar />
+    <BrowserRouter className={`App ${theme}`}>
+      <Navbar toggleTheme={toggleTheme}/>
       <Routes>
         <Route path="/" element={<Home countries={countries} />} />
         <Route path='/:countryName' element={<Country countries={countries}/>} />
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,10 +1,11 @@
 import React from 'react'
 
-export default function Navbar() {
+export default function Navbar({ toggleTheme }) {
+
     return (
         <div className="navbar">
             <h3>Where in the world?</h3>
-            <div className="dark-mode-toggle">
+            <div className="dark-mode-toggle" onClick={toggleTheme}>
                 <ion-icon name="moon-outline"></ion-icon>
                 <p>Dark Mode</p>
             </div>
