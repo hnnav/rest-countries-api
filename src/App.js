3748fc58aa57fe19d99f8906b6fdd64317edebb9
hnnav <57915266+hnnav@users.noreply.github.com>
2022-11-03 14:52:38 +0200

    fixes2

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import './styles/App.css';
-import './styles/darkMode.css';
+import './styles/Darkmode.css';
 import './styles/Desktop.css';
 import Navbar from './components/Navbar'
 import Home from './components/Home'
